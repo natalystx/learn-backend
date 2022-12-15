0188cbae16877939ba47a5d21d99156175525a94
natalyst <nattapong.anuwong@gmail.com>
2022-12-15 11:58:20 +0700

    add change password

--- src/User/index.ts
+++ src/User/index.ts
@@ -32,17 +32,42 @@ export default class User {
     `
     );
 
+    if (!(result as UserParams[]).length) {
+      throw new Error("User doesn't exist");
+    }
     return result as UserParams[];
   }
 
   async register(param: UserParams) {
     if (!this.db) return;
-    const [result] = await this.db?.query(
+    await this.db?.query(
       `
         INSERT INTO users (username, password) VALUES ('${param.username}', '${param.password}')
     `
     );
 
+    return this.findUser(param);
+  }
+
+  async changePassword(param: UserParams & { oldPassword: string }) {
+    const { username, password, oldPassword } = param;
+    const user = await this.findUser({ username, password: oldPassword });
+
+    if (!user) {
+      throw new Error("User doesn't exist");
+    }
+
+    const result = await this.db?.execute(
+      `UPDATE users SET password='${password}' WHERE username='${username}'`
+    );
+
+    return result;
+  }
+
+  async delete(username: string) {
+    const result = await this.db?.execute(
+      `DELETE FROM users WHERE username='${username}'`
+    );
     return result;
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -11,12 +11,9 @@ const initDatabase = async () => {
 };
 
 let user: User;
-
-let isLoggedIn = false;
-app.use(bodyParser.json()); // to support JSON-encoded bodies
+app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
-    // to support URL-encoded bodies
     extended: true,
   })
 );
@@ -24,10 +21,22 @@ app.use(
 app.listen("4002", async () => {
   await initDatabase();
   user = new User(db);
-  const res = await user.findUser({
-    username: "admin",
-    password: "12345",
-  });
+});
+
+app.post("/register", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    if (!user) {
+      throw new Error("User class found");
+    }
+
+    const result = await user.register({ username, password });
+    res.json({ result });
+  } catch (err) {
+    res.status(403);
+    res.json({ err });
+  }
 });
 
 app.post<UserParams>("/login", async (req, res) => {
@@ -49,4 +58,36 @@ app.post<UserParams>("/login", async (req, res) => {
     res.status(404);
     res.send({ error: "User doesn't exist" });
   }
+});
+
+app.patch<UserParams & { oldPassword: string }>(
+  "/user/change-password",
+  async (req, res) => {
+    if (!user) {
+      throw new Error("User class found");
+    }
+
+    const { username, password, oldPassword } = req.body;
+    try {
+      await user.changePassword({
+        username,
+        password,
+        oldPassword,
+      });
+      res.json({ success: true });
+    } catch (error) {
+      res.status(403);
+      res.json(error);
+    }
+  }
+);
+
+app.delete<{ username: string }>("/user/delete/:username", async (req, res) => {
+  const { username } = req.params;
+
+  if (!user) {
+    throw new Error("User class found");
+  }
+  await user.delete(username);
+  res.json({ success: true });
 });
